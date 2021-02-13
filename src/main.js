e158488c5e1d6c5b797be7b2b93f0a6c4e0b3ef1
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-13 15:35:02 -0500

    Add Tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/styles/index.css'
+
+// Create the app
+let app = createApp(App);
+
+// Mount the app
+app.mount('#app');
