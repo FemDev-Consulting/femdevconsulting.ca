7428bdae11ebaa069a93b344abc968ef58ec1217
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-15 10:13:23 -0500

    Fix responsive styling

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="relative z-10 overflow-hidden">
+    <div class="container">
+      <div class="w-full lg:w-2/3 py-40 md:py-52 md:ml-10 xl:ml-40 bg-gray-200s ">
+        <div class="sm:text-center lg:text-left">
+          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span class="block xl:inline">Data to enrich your</span>
+            <span class="block text-indigo-600 xl:inline">online business</span>
+          </h1>
+          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          </p>
+          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                Get started
+              </a>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                Live demo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden absolute bottom-0 top-0 right-0 lg:flex items-center w-1/2">
+      <svg
+        class="transform translate-x-1/2 w-full fill-current text-indigo-50"
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M36.5,-58.5C45.5,-51,49.8,-37.8,51.8,-25.9C53.9,-13.9,53.6,-3.1,54.3,9.4C55,22,56.7,36.3,52.2,49.9C47.6,63.5,36.9,76.4,24,79C11.1,81.5,-4.1,73.6,-14.9,64.3C-25.6,54.9,-32,44.1,-42.6,35.1C-53.2,26.1,-68.2,19,-74.7,7.6C-81.3,-3.9,-79.6,-19.7,-70.8,-29.6C-62.1,-39.6,-46.3,-43.6,-33.4,-49.3C-20.4,-55,-10.2,-62.4,1.8,-65.1C13.7,-67.9,27.5,-66,36.5,-58.5Z" transform="translate(100 100)" />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+
+  }
+}
+</script>
