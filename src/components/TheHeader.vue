337788c5c2b0e9d46e302d7e6b84afc138a4e3e0
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-15 13:28:13 -0500

    Add logo and brand name

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,19 @@
       <div class="container">
         <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div class="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span class="sr-only">Workflow</span>
-              <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+            <a
+              class="flex items-center"
+              href="#">
+              <svg
+                class="h-8 w-auto sm:h-10 text-brand-green-600"
+                viewBox="0 0 211 211" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <!-- fill="#F5CFCE" stroke="#DEBBBA" -->
+                <circle cx="105.5" cy="105.5" r="105" class=" fill-current" />
+                <rect x="74" y="62" width="71" height="17" fill="white"/>
+                <rect x="74" y="103" width="66" height="17" fill="white"/>
+                <rect x="74" y="141" width="17" height="17" fill="white"/>
+              </svg>
+              <span class=" ml-5 text-2xl font-semibold tracking-widest text-brand-green-600">FEMDEV</span>
             </a>
           </div>
           <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
@@ -16,7 +26,7 @@
               Sign in
             </a>
             -->
-            <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-brand-green-600 hover:bg-brand-green-700">
               Contact us
             </a>
           </div>
