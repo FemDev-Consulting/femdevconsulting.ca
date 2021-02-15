04de0251e9cfede5e78a2b5720a8ac75b65e7550
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-15 14:00:28 -0500

    Fix padding on md

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative overflow-hidden z-20">
     <div class="container">
-      <div class="w-full lg:w-2/3 py-40 md:py-52 lg:py-40 md:ml-10 xl:ml-40 bg-gray-200s ">
+      <div class="w-full lg:w-2/3 py-40 md:py-52 lg:py-40 lg:ml-10 xl:ml-40 bg-gray-200s ">
         <div class="sm:text-center lg:text-left">
           <h1 class="text-4xl tracking-tight font-extrabold text-brand-green-800 sm:text-5xl md:text-6xl xl:text-7xl">
             Data to enrich your online business
