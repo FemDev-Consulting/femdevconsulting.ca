8bd58367ea4742cc4d7a2582cb7ccfd23557bc8c
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-15 20:03:22 -0500

    Adjust styles of About section

--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="bg-gradient-to-b from-brand-green-50 to-white">
+  <div class=" bg-brand-green-50">
     <div class="container">
-      <div class="py-40 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div class="bg-brand-pink-600 flex items-center">
+      <div class="py-40 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24 lg:items-center">
+        <div class="bg-brand-pink-600 flex items-center py-64 ">
           Hello
         </div>
-        <div class="">
-          <h2 class="lg:text-3xl font-bold text-brand-green-800">
+        <div>
+          <h2 class="lg:text-2xl font-bold text-brand-green-800">
             About us
           </h2>
-          <p class="mt-6 text-4xl text-brand-green-600 font-bold">
-            Etiam eros nisl, eleifend nec magna nec, tincidunt eleifend exe
+          <p class="mt-2 text-4xl text-gray-900 font-extrabold">
+            Used by the world's most average companies
           </p>
-          <p class="mt-6 text-2xl text-brand-green-600 leading-relaxed">
+          <p class="mt-6 text-xl  text-gray-600  leading-relaxed">
             Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
           </p>
+          <!--
           <div class="mt-6">
             <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-green-600 hover:bg-brand-green-800">
               Get started
             </a>
           </div>
+          -->
         </div>
       </div>
     </div>
