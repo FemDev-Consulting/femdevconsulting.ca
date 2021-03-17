cf7b2ce467becc7f00091f286adb8c68b37e84f9
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-17 19:36:02 -0400

    Add logos and update hero clients section styles

--- src/components/HomeClients.vue
+++ src/components/HomeClients.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="bg-white">
-      <div class="container py-12 lg:py-16">
+      <div class="container py-12 lg:py-52">
         <div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
           <div>
             <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              Used by the world's most average companies
+              Our Clients
             </h2>
             <p class="mt-3 max-w-3xl text-lg text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.
+              We are proud to partner with organizations that are committed to advancing
+              gender equality, social justice and human rights.
             </p>
           </div>
           <div class="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Workcation">
+              <img class="max-h-12" :src="imgData2xUrl" alt="Workcation">
+            </div>
+            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+              <img class="max-h-12" :src="imgGatesUrl" alt="Workcation">
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
@@ -40,8 +43,17 @@
 </template>
 
 <script>
+import imgGatesUrl from './../assets/images/clients/bill-and-melinda-gates-foundation.jpg';
+import imgData2xUrl from './../assets/images/clients/data2x.png';
+
+
 export default {
   setup () {
+
+    return {
+      imgGatesUrl,
+      imgData2xUrl,
+    }
 
   }
 }
