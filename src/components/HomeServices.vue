897114aa5b0dbe3294ac23c0f42531f2ec8bcc3c
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-17 19:37:01 -0400

    Refactor services using v-for loop

--- src/components/HomeServices.vue
+++ src/components/HomeServices.vue
@@ -3,69 +3,27 @@
     <div class="bg-brand-green-700">
       <div class="md:container py-40 lg:w-5/6 ">
         <h2 class="text-4xl font-extrabold text-white tracking-tight">
-          Inbox support built for efficiency
+          Our Services
         </h2>
          <p class="mt-4 max-w-3xl text-xl text-brand-green-200">
-          Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis. Blandit aliquam sit nisl euismod mattis in.
+          Our expertise in gender equality and women’s rights is diverse, ranging from education and decent employment, to health and gender-based violence, to social norms and care work. We work with organizations to provide the following types of services:
         </p>
         <div class="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16">
 
-          <div>
+          <div
+            v-for="service in services"
+            :key="service.title" >
             <div class="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10 shadow-sm">
               <!-- Heroicon name: outline/inbox -->
-              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-              </svg>
+              <div v-html="service.svg">
+              </div>
             </div>
             <div class="mt-6">
-              <h3 class="text-xl font-medium text-white">Unlimited Inboxes</h3>
+              <h3 class="text-xl font-medium text-white">
+                {{ service.title }}
+              </h3>
               <p class="mt-2 text-base text-brand-green-200">
-                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10 shadow-sm">
-              <!-- Heroicon name: outline/inbox -->
-              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-              </svg>
-            </div>
-            <div class="mt-6">
-              <h3 class="text-xl font-medium text-white">Unlimited Inboxes</h3>
-              <p class="mt-2 text-base text-brand-green-200">
-                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10 shadow-sm">
-              <!-- Heroicon name: outline/inbox -->
-              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-              </svg>
-            </div>
-            <div class="mt-6">
-              <h3 class="text-xl font-medium text-white">Unlimited Inboxes</h3>
-              <p class="mt-2 text-base text-brand-green-200">
-                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10 shadow-sm">
-              <!-- Heroicon name: outline/inbox -->
-              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-              </svg>
-            </div>
-            <div class="mt-6">
-              <h3 class="text-xl font-medium text-white">Unlimited Inboxes</h3>
-              <p class="mt-2 text-base text-brand-green-200">
-                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                {{ service.description }}
               </p>
             </div>
           </div>
@@ -79,6 +37,78 @@
 <script>
 export default {
   setup () {
+
+    const serviceSvgClass = 'h-6 w-6 text-white';
+
+    const services = [
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Research',
+        description: 'Design, facilitation and analysis of research on gender equality and women’s empowerment ',
+      },
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Evaluation',
+        description: 'Evaluation of development programs, including the design of instruments and indicators',
+      },
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Analysis',
+        description: 'Evidence reviews and landscaping analysis of development issues, programs and trends',
+      },
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Knowledge translation',
+        description: 'Translating technical data and evidence into alternative knowledge products',
+      },
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Event facilitation',
+        description: 'Facilitation of training seminars, workshops, and public learning events',
+      },
+      {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Proposal development',
+        description: 'Project and grant proposal development',
+      },
+       {
+        svg: `
+          <svg class="${serviceSvgClass}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
+          `,
+        title: 'Strategic planning',
+        description: 'Organizational strategic planning, evaluation and learning',
+      }
+    ];
+
+    return {
+      services,
+    }
 
   }
 }
