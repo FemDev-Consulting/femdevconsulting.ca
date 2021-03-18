2a50373beaaca9cd506bd92f3e42710a33c19d11
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-18 00:12:20 -0400

    Add client logos to public and updates styles

--- src/components/HomeClients.vue
+++ src/components/HomeClients.vue
@@ -1,58 +1,46 @@
 <template>
-  <div>
-    <div class="bg-white">
-      <div class="container py-12 lg:py-52">
-        <div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
-          <div>
-            <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              Our Clients
-            </h2>
-            <p class="mt-3 max-w-3xl text-lg text-gray-500">
-              We are proud to partner with organizations that are committed to advancing
-              gender equality, social justice and human rights.
-            </p>
-          </div>
-          <div class="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" :src="imgData2xUrl" alt="Workcation">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" :src="imgGatesUrl" alt="Workcation">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
-            </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-              <img class="max-h-12" src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg" alt="Statamic">
-            </div>
-          </div>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="bg-white">
+    <div class="container mb-28 py-12 lg:px-20 w-2/3 ">
+      <p class="mb-20 text-center text-xl font-semibold uppercase text-brand-green-500 tracking-wider">
+        We are proud to partner with organizations that are committed to advancing gender equality,
+        social justice and human rights.
+      </p>
+      <div class="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-4 lg:gap-y-20 items-center">
+        <div class=" col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <img class="h-12 lg:h-8 object-contain" src="/images/clients/bill-and-melinda-gates-foundation.jpg" alt="">
+        </div>
+        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <img class="h-12 lg:h-8 object-contain" src="/images/clients/data2x.png" alt="">
+        </div>
+        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <img class="h-12 lg:h-8 object-contain" src="/images/clients/global-affairs-canada.jpeg" alt="">
+        </div>
+        <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+          <img class="h-20 object-contain" src="/images/clients/idrc.png" alt="">
+        </div>
+        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <img class="h-20 object-contain" src="/images/clients/islamic-relief-canada.png" alt="">
+        </div>
+        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <img class="h-12 lg:h-10 object-contain" src="/images/clients/malala-fund.png" alt="">
+        </div>
+        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <img class="h-12 object-contain" src="/images/clients/oxfam-canada.png" alt="">
+        </div>
+        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <img class="h-20 object-contain" src="/images/clients/urban-institute.jpg" alt="">
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import imgGatesUrl from './../assets/images/clients/bill-and-melinda-gates-foundation.jpg';
-import imgData2xUrl from './../assets/images/clients/data2x.png';
-
-
 export default {
   setup () {
 
     return {
-      imgGatesUrl,
-      imgData2xUrl,
     }
 
   }
