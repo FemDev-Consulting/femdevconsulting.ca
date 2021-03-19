4c4699b5f9e82dbc0026e22531d664a325786dff
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-19 17:19:09 -0400

    Centre header text

--- src/components/HomeClients.vue
+++ src/components/HomeClients.vue
@@ -1,11 +1,20 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="bg-white">
-    <div class="container mb-28 py-12 lg:px-20 w-2/3 ">
+    <div class="container mt-20 mb-28 py-12 lg:px-20 w-2/3 ">
+      <h2 class="text-5xl font-extrabold text-brand-green-800 tracking-tight text-center">
+        Our Clients
+      </h2>
+      <p class="my-14 text-2xl text-gray-600 max-w-prose text-center">
+        We are proud to partner with organizations that are committed to advancing gender equality,
+        social justice and human rights.
+      </p>
+      <!--
       <p class="mb-20 text-center text-xl font-semibold uppercase text-brand-green-500 tracking-wider">
         We are proud to partner with organizations that are committed to advancing gender equality,
         social justice and human rights.
       </p>
+      -->
       <div class="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-4 lg:gap-y-20 items-center">
         <div class=" col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
           <img class="h-12 lg:h-8 object-contain" src="/images/clients/bill-and-melinda-gates-foundation.jpg" alt="">
