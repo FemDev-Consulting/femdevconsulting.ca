e56a994bb9a3530fd2e2564aa16368fbab50c4ca
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-19 17:18:56 -0400

    Add bubble and restyle text

--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,31 +1,25 @@
 <template>
-  <div class=" bg-white">
-    <div class="container">
-      <div class="py-40 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24 lg:items-center">
-        <div class="bg-brand-pink-600 flex items-center py-64 ">
-          Hello
-        </div>
-        <div>
-          <h2 class="lg:text-4xl font-bold text-brand-green-800">
+  <div class="relative overflow-hidden z-20">
+    <div class="container flex flex-row-reverse">
+      <div class="w-full lg:w-2/5 py-40 md:py-52 lg:py-52 lg:ml-10 xl:ml-40">
+        <div class="sm:text-center lg:text-left">
+          <h1 class="tracking-normal font-extrabold text-brand-green-800 text-4xl sm:text-5xl md:text-6xl xl:text-5xl">
             About us
-          </h2>
-          <!--
-          <p class="mt-2 text-4xl text-gray-900 font-extrabold">
-            Sub title
-          </p>
-          -->
-          <p class="mt-6 text-xl  text-gray-600  leading-relaxed">
+          </h1>
+          <p class="mt-6 text-2xl  text-gray-600  leading-relaxed max-w-prose">
             FemDev is an Ottawa-based global development consulting firm providing feminist research, evaluation and analysis services. We partner with organizations that are committed to advancing gender equality, social justice and human rights.
           </p>
-          <!--
-          <div class="mt-6">
-            <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-green-600 hover:bg-brand-green-800">
-              Get started
-            </a>
-          </div>
-          -->
+
         </div>
       </div>
+    </div>
+    <div class="hidden absolute bottom-0 top-0 left-0 lg:flex items-center w-1/2" style="z-index: -1;">
+      <svg
+        class="transform rotate-45 scale-105 xl:scale-105 -translate-x-1/3 2xl:-translate-x-1/2 w-full fill-current text-brand-pink-600"
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M56.3,-42.2C71.2,-26.2,80.4,-2.9,76,17.6C71.5,38,53.4,55.6,32.9,63.7C12.5,71.8,-10.4,70.4,-28.8,61C-47.3,51.7,-61.4,34.5,-68.1,13.2C-74.8,-8,-74.2,-33.3,-61.6,-48.8C-49,-64.3,-24.5,-70.1,-1.9,-68.6C20.7,-67.1,41.3,-58.3,56.3,-42.2Z" transform="translate(100 100)" />
+      </svg>
     </div>
   </div>
 </template>
