e9aec0fa457204ae28cabd37c05028690a6b6aff
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-21 17:38:34 -0400

    Remove the phone field

--- src/components/HomeContact.vue
+++ src/components/HomeContact.vue
@@ -5,38 +5,40 @@
       <div class="flex flex-row flex-nowrap">
         <!-- First column -->
         <div class="w-1/2">
-          <h2 class="text-5xl font-extrabold text-brand-green-800 tracking-tight">
-            Get in touch
-          </h2>
-          <p class="my-10 text-2xl text-gray-600 max-w-prose">
-            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.
-          </p>
-          <dl class="mt-8 text-base text-gray-500">
-            <div class="mt-6">
-              <dt class="sr-only">Phone number</dt>
-              <dd class="flex">
-                <!-- Heroicon name: outline/phone -->
-                <svg class="flex-shrink-0 h-8 w-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                </svg>
-                <span class="ml-3 text-xl">
-                  +1 (555) 123-4567
-                </span>
-              </dd>
-            </div>
-            <div class="mt-3">
-              <dt class="sr-only">Email</dt>
-              <dd class="flex">
-                <!-- Heroicon name: outline/mail -->
-                <svg class="flex-shrink-0 h-8 w-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
-                <span class="ml-3 text-xl">
-                  support@example.com
-                </span>
-              </dd>
-            </div>
-          </dl>
+          <div class="pr-10">
+            <h2 class="text-5xl font-extrabold text-brand-green-800 tracking-tight">
+              Get in touch
+            </h2>
+            <p class="my-10 text-2xl text-gray-600 max-w-prose">
+              Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.
+            </p>
+            <dl class="mt-8 text-base text-gray-500">
+              <div class="mt-6">
+                <dt class="sr-only">Phone number</dt>
+                <dd class="flex">
+                  <!-- Heroicon name: outline/phone -->
+                  <svg class="flex-shrink-0 h-8 w-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                  </svg>
+                  <span class="ml-3 text-xl">
+                    +1 (555) 123-4567
+                  </span>
+                </dd>
+              </div>
+              <div class="mt-3">
+                <dt class="sr-only">Email</dt>
+                <dd class="flex">
+                  <!-- Heroicon name: outline/mail -->
+                  <svg class="flex-shrink-0 h-8 w-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                  </svg>
+                  <span class="ml-3 text-xl">
+                    support@example.com
+                  </span>
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
          <!-- Second column -->
          <div class="w-1/2">
@@ -49,10 +51,6 @@
               <div>
                 <label for="email" class="sr-only">Email</label>
                 <input id="email" name="email" type="email" autocomplete="email" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Email">
-              </div>
-              <div>
-                <label for="phone" class="sr-only">Phone</label>
-                <input type="text" name="phone" id="phone" autocomplete="tel" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Phone">
               </div>
               <div>
                 <label for="message" class="sr-only">Message</label>
