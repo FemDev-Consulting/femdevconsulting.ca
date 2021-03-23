79f898bf10ac05bac7655e6644727e43fbe22fd0
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-23 09:09:27 -0400

    Adjust styling

--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="relative overflow-hidden z-20">
     <div class="container flex justify-center">
-      <div class="w-full lg:w-1/2 py-40 md:py-52 lg:py-60 lg:ml-10 xl:ml-40">
+      <div class="w-full lg:w-1/2 py-40 md:py-52 lg:py-52 lg:ml-10 xl:ml-40">
         <div class="sm:text-center lg:text-left">
-          <h1 class="tracking-normal font-bold text-brand-green-800 text-4xl sm:text-5xl md:text-6xl xl:text-6xl">
+
+          <h2 class="tracking-normal font-bold text-brand-green-800 text-4xl sm:text-5xl md:text-6xl xl:text-6xl">
             About us
-          </h1>
+          </h2>
           <p class="mt-10 text-2xl text-gray-900 leading-relaxed font-light max-w-prose">
             FemDev is an Ottawa-based global development consulting firm providing feminist research, evaluation and analysis services. We partner with organizations that are committed to advancing gender equality, social justice and human rights.
           </p>
@@ -15,7 +16,7 @@
     </div>
     <div class="hidden absolute bottom-0 top-0 left-0 lg:flex items-center w-1/2" style="z-index: -1;">
       <svg
-        class="transform rotate-45 scale-105 xl:scale-105 -translate-x-1/3 2xl:-translate-x-1/2 w-full fill-current text-brand-pink-600"
+        class="transform rotate-45 scale-105 xl:scale-90 -translate-x-1/3 2xl:-translate-x-1/2 w-full fill-current text-brand-pink-600"
         viewBox="0 0 200 200"
         xmlns="http://www.w3.org/2000/svg">
         <path d="M56.3,-42.2C71.2,-26.2,80.4,-2.9,76,17.6C71.5,38,53.4,55.6,32.9,63.7C12.5,71.8,-10.4,70.4,-28.8,61C-47.3,51.7,-61.4,34.5,-68.1,13.2C-74.8,-8,-74.2,-33.3,-61.6,-48.8C-49,-64.3,-24.5,-70.1,-1.9,-68.6C20.7,-67.1,41.3,-58.3,56.3,-42.2Z" transform="translate(100 100)" />
--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -13,7 +13,12 @@
           -->
           <div class="mt-5 sm:mt-8 md:mt-20 sm:flex sm:justify-center lg:justify-start">
             <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-brand-green-600 hover:bg-brand-green-700 md:py-4 md:text-lg md:px-10">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base
+                font-medium rounded-md text-white
+                bg-brand-green-600 hover:bg-brand-green-700
+                md:py-4 md:text-lg md:px-10">
                 Work With Us
               </a>
             </div>
