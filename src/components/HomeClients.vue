f42b2769160259619b17020ff920be649680d17f
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-24 16:40:48 -0400

    Fix height of Gates image

--- src/components/HomeClients.vue
+++ src/components/HomeClients.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="bg-white">
-    <div class="container mt-20 mb-28 py-12 lg:px-20 md:w-2/3">
+    <div class="container mt-20 mb-20 py-12 lg:px-20 md:w-2/3">
 
-     <h2 class="tracking-normal font-bold text-brand-green-800 text-4xl sm:text-5xl md:text-6xl xl:text-5xl md:text-center">
+      <h2 class="tracking-normal font-bold text-brand-green-800 text-4xl sm:text-5xl md:text-6xl xl:text-5xl md:text-center">
         Our Clients
       </h2>
       <p class="mt-10 text-xl text-gray-700 leading-loose max-w-prose md:text-center mx-auto">
@@ -17,32 +17,43 @@
         social justice and human rights.
       </p>
       -->
-      <div class="mt-20 grid grid-cols-1 md:grid-cols-6 lg:grid-cols-2 lg:gap-y-20 items-center">
-        <div class="flex justify-center bg-red-100">
-          <img class="h-22 lg:h-8 object-contain" src="/images/clients/bill-and-melinda-gates-foundation.jpg" alt="">
-        </div>
-        <div class="flex justify-center bg-gray-600">
+
+      <ul class="mx-auto md:mx-0 mt-20 w-1/2 md:w-auto grid gap-10 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+
+        <li class="flex justify-center">
+          <img class="h-12 lg:h-8 object-contain" src="/images/clients/bill-and-melinda-gates-foundation.jpg" alt="">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-12 lg:h-8 object-contain" src="/images/clients/data2x.png" alt="">
-        </div>
-        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-12 lg:h-8 object-contain" src="/images/clients/global-affairs-canada.jpeg" alt="">
-        </div>
-        <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-20 object-contain" src="/images/clients/idrc.png" alt="">
-        </div>
-        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-20 object-contain" src="/images/clients/islamic-relief-canada.png" alt="">
-        </div>
-        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-12 lg:h-10 object-contain" src="/images/clients/malala-fund.png" alt="">
-        </div>
-        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-12 object-contain" src="/images/clients/oxfam-canada.png" alt="">
-        </div>
-        <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+        </li>
+
+        <li class="flex justify-center">
           <img class="h-20 object-contain" src="/images/clients/urban-institute.jpg" alt="">
-        </div>
-      </div>
+        </li>
+
+      </ul>
+
     </div>
   </div>
 </template>
